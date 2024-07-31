687060ee401f13138e692f9ad1137e38505de86e
Rizki Daulah <rizkidaulah266@gmail.com>
2024-08-01 02:20:20 +0700

    update feature last

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-//INI JS
+//INI Js
+
+//formvalidation
+function formValidation(){
+    let name = document.getElementById('input-name').value;
+    console.log(name);
+
+    let email = document.getElementById('input-email').value;
+    console.log(email);
+
+    if (name == '' || null){
+        alert('Maaf nama anda belum di isi');
+    } else {
+        prompt("apa benar ini data anda ?", (name));
+    }
+    
+    if (email == ''){
+        alert('Maaf email anda belum di isi');
+    } else {
+        prompt("apa benar ini data anda ?", (email));
+    }  
+
+}
